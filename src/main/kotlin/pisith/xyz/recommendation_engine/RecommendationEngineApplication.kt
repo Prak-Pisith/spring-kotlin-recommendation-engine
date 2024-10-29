068dc53a049d73cb318c10802af625ffdfbac52a
pisithprak <pisith.p-external@webtiga.com>
2024-10-29 07:23:36 +0700

    first commit

---
+++ src/main/kotlin/pisith/xyz/recommendation_engine/RecommendationEngineApplication.kt
@@ -0,0 +1,11 @@
+package pisith.xyz.recommendation_engine
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RecommendationEngineApplication
+
+fun main(args: Array<String>) {
+	runApplication<RecommendationEngineApplication>(*args)
+}
---
+++ src/test/kotlin/pisith/xyz/recommendation_engine/RecommendationEngineApplicationTests.kt
@@ -0,0 +1,13 @@
+package pisith.xyz.recommendation_engine
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RecommendationEngineApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
